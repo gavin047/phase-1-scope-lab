9e42f7c893a822edf95e3227e2007eb8f034a10d
gavin <gavinmichori@gmail.com>
2024-07-08 11:09:37 +0300

    .

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';  
+  
+function upperCaseCustomerName() {  
+  return customerName.toUpperCase();  
+}  
+  
+function setBestCustomer() {  
+  bestCustomer = 'not bob';  
+}  
+  
+function overwriteBestCustomer() {  
+  bestCustomer = 'aybe bob';  
+}  
+  
+const leastFavoriteCustomer = 'alice';  
+  
+function changeLeastFavoriteCustomer() {  
+  leastFavoriteCustomer = 'charlie'; // This will throw an error  
+}  
+
